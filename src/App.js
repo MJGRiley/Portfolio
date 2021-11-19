f6879193bc4fa45ae0c2e821440bda34c8d8f994
MJGR <mjgriley@gmail.com>
2021-11-18 19:43:48 -0500

    basics are implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from "react"
+import PortfolioContainer from "./components/PortfolioContainer"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
+//   return (
+//     <div className="App">
+//       <nav>WHEN I view the navigation titles
+// THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted</nav>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+        <a href='mailto:mjgriley@gmail.com'>
+        MJGRiley@gmail.com
+        </a>
+      </p>
+    </div>
+  );
+}

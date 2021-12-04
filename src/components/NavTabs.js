99fa038a1a67dfaa4e75645d281b1a5475b5f101
MJGR <mjgriley@gmail.com>
2021-12-03 23:41:34 -0500

    added repo and some padding

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -46,6 +46,15 @@ function NavTabs({ currentPage, handlePageChange }) {
           Contact
         </a>
       </li>
+      <li className="nav-item">
+        <a
+          href="https://github.com/MJGRiley"
+          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className='nav-link'
+        >
+          GitHub: MJGRiley
+        </a>
+      </li>
     </ul>
   );
 }
